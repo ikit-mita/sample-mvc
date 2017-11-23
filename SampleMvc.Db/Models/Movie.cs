658cb372983918dfd0a58f9ed230f09f3ad8c21d
Denis Zavershinskiy <zaverden@gmail.com>
2017-11-23 20:20:05 +0700

    add movies

---
+++ SampleMvc.Db/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleMvc.Db.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Вам надо заполнить {0}.")]
+        [MaxLength(64)]
+        [Display(Name = "Заголовок")]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [MaxLength(1024)]
+        public string Description { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Genre { get; set; }
+        public decimal TicketPrice { get; set; }
+    }
+}
